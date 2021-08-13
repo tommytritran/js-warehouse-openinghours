f143716f5b5b24149f0a5cdf4718f6cb78958c15
Tommy Tran <tommy.tran@inmeta.no>
2021-08-13 12:17:37 +0200

    initial frontend setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="p-6 bg-primary max-w-lg mx-auto mt-8 rounded-xl shadow-md flex items-center">
+      <h1 className="text-white font-medium">Warehouse openinghours</h1>
+    </div>
+  );
+}
+
+export default App;
