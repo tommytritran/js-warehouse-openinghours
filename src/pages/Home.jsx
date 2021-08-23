3ddbe2108967381d65a9f11de02021bdc4af0c0b
Tommy Tran <tommy.tran@inmeta.no>
2021-08-23 10:37:23 +0200

    created home.jsx component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PeriodList from '../components/period/PeriodList';
+
+export default function Home() {
+  return (
+    <div className="lg:px-12 py-4 bg-gray-300">
+      <PeriodList />
+    </div>
+  );
+}
