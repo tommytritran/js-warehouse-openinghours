a7684596e334eeaf931b6d251f33fcd15ef40cec
Tommy Tran <tommy.tran@inmeta.no>
2021-08-26 10:04:32 +0200

    added navbar to index page

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Navbar from './components/layout/Navbar';
 import './index.css';
 import Home from './pages/Home';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Navbar />
     <Home />
   </React.StrictMode>,
   document.getElementById('root'),
