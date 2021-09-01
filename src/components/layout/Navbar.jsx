bcaf8a44283dfa2bcd924ca74f507e7e9348e877
Tommy Tran <tommy.tran@inmeta.no>
2021-09-01 15:25:49 +0200

    added routing

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <div className="px-8 py-4 bg-indigo-400">
-      <h1>Navbar</h1>
+    <div className="flex space-x-4 px-8 py-4 bg-indigo-400 text-white">
+      <Link to="/">Home</Link>
+      <Link to="/calendar">Calendar</Link>
     </div>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import './index.css';
+import Calendar from './pages/Calendar';
 import Home from './pages/Home';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Navbar />
-    <Home />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <div className="bg-gray-100 min-h-screen">
+        <Navbar />
+        <div className="xs:px-6 xs:py-8 lg:px-8 lg:py-4">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/calendar" component={Calendar} />
+          </Switch>
+        </div>
+      </div>
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root'),
 );
