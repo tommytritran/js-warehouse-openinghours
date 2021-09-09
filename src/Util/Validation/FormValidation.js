fd1484288ace237e7981d0562367fa88eb4156f6
Tommy Tran <tommy.tran@inmeta.no>
2021-09-09 12:56:14 +0200

    implemented basic form validation

--- src/Util/Validation/FormValidation.js
+++ src/Util/Validation/FormValidation.js
@@ -2,6 +2,7 @@
 const periodSchema = {
   warehouse: (value) => /^([ÆØÅæøåA-Za-z]+)$/.test(value),
   priority: (value) => parseInt(value, 10) === Number(value),
+  // TODO add all period attributes to validate
 };
 
 export function validatePeriod(period) {
